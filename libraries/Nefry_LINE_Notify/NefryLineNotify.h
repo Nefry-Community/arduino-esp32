11b29368ec2d5eb3d85bc9acd72cf6e2c0466f32
wamisnet <wamiwami@live,jp>
2018-04-20 21:13:55 +0900

    LineNotify対応

---
+++ libraries/Nefry_LINE_Notify/NefryLineNotify.h
@@ -0,0 +1,18 @@
+﻿#ifndef NefryLINENotify_h
+#define NefryLINENotify_h
+
+#include <WiFiClientSecure.h>
+#include <Nefry.h>
+
+class Nefry_LINE_Notify
+{
+public:
+	bool
+		send(String auth, String message);
+private:
+	String
+		escapeParameter(String param),
+		StrPerEncord(const char* c_str);
+};
+extern Nefry_LINE_Notify LineNotify;
+#endif
